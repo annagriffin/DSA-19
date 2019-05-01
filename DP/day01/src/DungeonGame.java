bca39d922b268ef9b740e11b763ee659ee7b7692
Anna Griffin <anna.griffin@students.olin.edu>
2019-04-30 21:50:49 -0400

    dp

--- DP/day01/src/DungeonGame.java
+++ DP/day01/src/DungeonGame.java
@@ -7,7 +7,11 @@ public class DungeonGame {
         int cols = map[0].length;
 
 
+
+
         int[][] DP = new int[rows][cols];
+
+
 
 
         // fill in amount needed to get to end from current row
@@ -16,17 +20,20 @@ public class DungeonGame {
 
         for (int i = rows-2; i >= 0; i--) {
             DP[i][cols-1] = Math.max(DP[i+1][cols-1]-map[i][cols-1], 1);
+
         }
 
+
+
         for (int j = cols-2; j >= 0; j--) {
-            DP[rows-1][j] = Math.max(DP[rows-1][cols+1]-map[rows-1][j], 1);
+            DP[rows-1][j] = Math.max(DP[rows-1][j+1]-map[rows-1][j], 1);
         }
 
         // traverse through and see how much each direction move would cost
         // take the minimum cost move.
 
         for (int i = rows-2; i >= 0; i--) {
-            for (int j = cols-2; i>=0; j--) {
+            for (int j = cols-2; j>=0; j--) {
 
                 int d = Math.max(DP[i+1][j]-map[i][j], 1);
                 int r = Math.max(DP[i][j+1]-map[i][j], 1);
